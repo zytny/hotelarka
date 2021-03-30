8ce1506e97bbb241e66e645cc68e4adbf8d54aa0
zytny <74960844+zytny@users.noreply.github.com>
2021-03-30 20:17:07 +0200

    HOT-16 rezerwacja frontend

--- rezerw.php
+++ rezerw.php
@@ -11,20 +11,65 @@
 		
 		
 		<title>Recepcja</title>
-		<link rel="stylesheet" href="styl.css">
+		<link rel="stylesheet" href="styld.css">
+		
+		<script>
+			function validate(){
+				var imie = document.getElementById('imie').value;
+				var nazwisko = document.getElementById('nazwisko').value;
+				var telefon = document.getElementById('telefon').value;
+				var mail = document.getElementById('mail').value;
+				var data1 = document.getElementById('datepicker1').value;
+				var data2 = document.getElementById('datepicker2').value;
+				var email_test = /^([A-Za-z0-9\-.]*\w)+@+([A-Za-z0-9\-.]*\w)+(\.[A-Za-z]*\w)+$/;
+				var test_result = mail.match(email_test);
+				
+				
+				if(imie.length < 3) {
+					alert("Podano nieprawidłowe imię.");
+					return false;
+				} else if(nazwisko.length < 3) {
+					alert("Podano nieprawidłowe nazwisko.");
+					return false;
+				} else if(telefon.length < 9 || telefon.length > 9) {
+					alert("Podano nieprawidłowy numer.");
+					return false;
+				} else if(data1.length < 10 || data1.length > 10) {
+					alert("Podano nieprawidłową datę.");
+					return false;
+				} else if(data2.length < 10 || data2.length > 10) {
+					alert("Podano nieprawidłową datę.");
+					return false;
+				} else if(test_result == null){
+					alert("Podano nieprawidłowy adres e-mail.");
+					return false;
+				} else {
+					return true;
+				}
+			}
+		</script>
 	</head>
 	<body>
-		<form method="POST" action="rezerwacja.php">
-		Imię: <input type="text" name="imie"><br>
-		Nazwisko: <input type="text" name="nazwisko"><br>
-		Numer telefonu: <input type="number" name="telefon"><br>
-		E-mail: <input type="text" name="mail"><br>
-		Termin: <input type="text" id="datepicker1" name="data1"><input type="text" id="datepicker2" name="data2">
-		<?php
-			$id = $_POST['identyfikator'];
-			echo "<input type='hidden' name='pokoj' value='$id'>"
-		?>
-		<br><input type="submit" value="REZERWUJ">
-		</form>
+		<div id="panel">
+			<form method="POST" action="rezerwacja.php" onsubmit="return validate();">
+			Imię:
+			<input type="text" name="imie" class="inp" id="imie"><br>
+			Nazwisko: 
+			<input type="text" name="nazwisko" class="inp" id="nazwisko"><br>
+			Numer telefonu: 
+			<input type="number" name="telefon" class="inp" id="telefon"><br>
+			E-mail: 
+			<input type="text" name="mail" class="inp" id="mail"><br>
+			Termin: <br>
+			<input type="text" id="datepicker1" name="data1" class="dat"><input type="text" id="datepicker2" name="data2" class="dat">
+			<?php
+				$id = $_POST['identyfikator'];
+				echo "<input type='hidden' name='pokoj' value='$id'>";
+			?>
+			<div id="lower">
+			<br><input type="submit" value="REZERWUJ">
+			</div>
+			</form>
+		</div>
 		<script src="kal.js"></script>
 	</body>
